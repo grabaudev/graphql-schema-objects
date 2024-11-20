3e48f564524d0d5da1d7a59606d200dac3ecbf34
Maximilian Grabau <max@grabau.dev>
2024-11-20 01:07:27 +0100

    feat: perform introspection query on command

--- src/Commands/GenerateCommand.php
+++ src/Commands/GenerateCommand.php
@@ -5,14 +5,52 @@ declare(strict_types=1);
 namespace GrabauDev\SchemaObjects\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+use Illuminate\Support\Facades\Http;
 
-class GenerateCommand extends Command
+class GenerateCommand extends Command implements PromptsForMissingInput
 {
-    protected $signature = 'schema-objects:generate';
+    protected $signature = 'schema-objects:generate {url}';
     protected $description = 'Generate schema objects';
 
     public function handle(): void
     {
-        $this->line('Generating schema objects...');
+        /** @var string $url */
+        $url = $this->argument('url');
+
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            $this->error('Invalid URL provided. Please enter a valid URL starting with http:// or https://.');
+            return;
+        }
+
+        $this->line('Running introspection on: ' . $url);
+
+        $schema = Http::post($url, [
+            'query' => '
+                query {
+                __schema {
+                        types {
+                            name
+                            fields {
+                                name
+                                type {
+                                    name
+                                }
+                            }
+                        }
+                    }
+                }
+            ',
+        ]);
+    }
+
+    /**
+     * @return array<string, array<string>>
+     */
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'url' => ['Which schema url do you want to inspect?', 'https://'],
+        ];
     }
 }
